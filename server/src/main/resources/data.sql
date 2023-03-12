1bd193d406be2d08b3fa4254f1d304714c48c152
Prasanna Dubey <47008212+luciferpd13@users.noreply.github.com>
2023-03-12 12:38:19 +0530

    Removing unnecessary files from spring boot

--- server/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
- CREATE TABLE NOTES (
-       id BIGINT NOT NULL AUTO_INCREMENT,
-       title varchar(255),
-       description varchar(255),
-       primary key (id)
- );
-INSERT INTO NOTES (title, description) VALUES
-  ('Yoga', 'Purify your Soul'),
-  ('Boxing', 'Great Sport');
